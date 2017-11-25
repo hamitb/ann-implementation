ddd3a244b9d64b288834af2af67cc786d1f6626b
Hamit Burak Emre <hamitburakemre@gmail.com>
2017-11-25 20:18:54 +0300

    Implement loss and relu

--- driver.py
+++ driver.py
@@ -15,3 +15,6 @@ np.random.seed(499)
 # Test the affine_forward function
 test_affine_forward()
 test_affine_backward()
+test_relu_forward()
+test_relu_backward()
+test_L2_loss()
--- layers.py
+++ layers.py
@@ -52,72 +52,58 @@ def affine_backward(dout, cache):
 
 
 def relu_forward(x):
-	"""
-	This function computes the forward pass for a layer of rectified linear units (ReLUs).
+    """
+    This function computes the forward pass for a layer of rectified linear units (ReLUs).
 
-	Input:
-	x: inputs, an array of any shape
+    Input:
+    x: inputs, an array of any shape
 
-	Outputs:
-	out: Output, an array of the same shape as x
-	cache: x
-	"""
+    Outputs:
+    out: Output, an array of the same shape as x
+    cache: x
+    """
 
-	out = None
-	#############################################################################
-	#                            START OF YOUR CODE                            #
-	#############################################################################
-	pass
-	#############################################################################
-	#                             END OF YOUR CODE                              #
-	#############################################################################
-	cache = x
-	return out, cache
+    out = np.maximum(0, x)
+    cache = x
+    return out, cache
 
 
 def relu_backward(dout, cache):
-	"""
-	This function computes the backward pass for a layer of rectified linear units (ReLUs).
+    """
+    This function computes the backward pass for a layer of rectified linear units (ReLUs).
 
-	Inputs:
-	dout: derivatives coming from the next layer, an array of any shape
-	cache: input x, an array of same shape as dout
+    Inputs:
+    dout: derivatives coming from the next layer, an array of any shape
+    cache: input x, an array of same shape as dout
 
-	Outputs:
-	dx: gradient with respect to x
-	"""
+    Outputs:
+    dx: gradient with respect to x
+    """
 
-	dx, x = None, cache
-	#############################################################################
-	#                            START OF YOUR CODE                            #
-	#############################################################################
-	pass
-	#############################################################################
-	#                             END OF YOUR CODE                              #
-	#############################################################################
-	return dx
+    dx, x = None, cache
+    dx = dout.copy()
+    dx[x <= 0] = 0
+    return dx
 
 
 def L2_loss(x, y):
-	"""
-	This functin computes the loss and gradient using L2 norm.
+    """
+    This functin computes the loss and gradient using L2 norm.
 
-	Inputs:
-	x: input data, an array of shape (N,) where x[i] is the regression output for the ith input.
-	y: vector of labels, an array of shape (N,) where y[i] is the label for x[i].
+    Inputs:
+    x: input data, an array of shape (N,) where x[i] is the regression output for the ith input.
+    y: vector of labels, an array of shape (N,) where y[i] is the label for x[i].
 
-	Outputs:
-	loss: scalar giving the loss
-	dx: gradient of the loss with respect to x
-	"""
-	loss = None
-	dx = None
+    Outputs:
+    loss: scalar giving the loss
+    dx: gradient of the loss with respect to x
+    """
+    loss = None
+    dx = None
 
-	#############################################################################
-	#                            START OF YOUR CODE                            #
-	#############################################################################
-	pass
-	#############################################################################
-	#                             END OF YOUR CODE                              #
-	#############################################################################
-	return loss, dx
+    N = x.shape[0]
+
+    loss = np.sum(1.0/(2*N) * np.square(y-x))
+    dx = -1.0/N * (y-x)
+    
+    return loss, dx
